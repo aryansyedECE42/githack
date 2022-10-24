039c07d983a2ce98f5acd0aa85af5fd884e7f96d
Saista-8451 <116583972+Saista-8451@users.noreply.github.com>
2022-10-24 23:11:05 +0530

    Add files via upload

---
+++ forloops.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class forloops {
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+int t=sc.nextInt();
+    for(int i=1; i<=10; i++){
+        System.out.println(t*i);
+    }
+  }  
+}
